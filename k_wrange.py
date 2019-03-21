d6f2a2910a9e89a0a8cd3dfe6cd62d059582d60e
Shahbaz <[5h4h842.c@gmail.com]>
2019-03-21 18:00:38 +0000

    2D Kvsalpha plot

--- k_wrange.py
+++ k_wrange.py
@@ -7,20 +7,10 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from scipy import special
 # This is a check for a git upload
 R1=0.5
-R2=1
-a1=np.linspace(0.01,4,600) # Bessel functions only take positive values
-a2=np.linspace(0.01,4,600) #Assures that the arrays have the same number of elements
-#a1,a2=np.meshgrid(a1,a2)
-#a1 = np.asarray(np.meshgrid(a1,a2))
-#a2 = a1
-#print('type(a2): ', type(a2))
-#print('type(a1): ', type(a1))
-#print(a1.shape)
-#print(a2.shape)
-#print('a1[4,4]: ', a1[4,4])
-#print('a2[4,4]: ', a2[4,4])
-#plt.scatter(a1,a2)
-#plt.show()
+R2=R1 # Bessel functions only take positive values
+a1=np.linspace(0.01,4,600) #Assures that the arrays have the same number of elements
+a2=a1
+
 
 scsp=scipy.special
 def J(v,a,R):
@@ -47,41 +37,19 @@ F1a2R2=J(1,abs(a2),R2)+(C2/B2)*Y(1,abs(a2),R2)
 
 K1=(2*np.pi/a1)*((R1**2)*(J(0,a1,R1)**2 + J(1,a1,R1)**2)
 	-2*R1*(1/a1)*J(0,a1,R1)*J(1,a1,R1))
-K2=((2*np.pi*B2**2)/a2)*(((F0a2R2**2) +(F1a2R2**2) -
-	(2*R2/a2)*F0a2R2*F1a2R2))
-K3=((2*np.pi*B2**2)/a2)*((R1**2)*((F0a2R1**2) +(F1a2R1**2)- 
-	(2*R1/a2)*F0a2R1*F1a2R1))
-K4=(4*np.pi*B2/a2)*(F0a2R1-F0a2R2)*R1*J(1,a1,R1)*(1/a1 - 1/a2)
+K2=((2*np.pi*B2**2)/abs(a2))*(((F0a2R2**2) +(F1a2R2**2) -
+	(2*R2/abs(a2))*F0a2R2*F1a2R2))
+K3=((2*np.pi*B2**2)/abs(a2))*(R1**2)*((F0a2R1**2) +(F1a2R1**2)- 
+	(2*R1/abs(a2)*F0a2R1*F1a2R1))
+K4=(4*np.pi*B2/abs(a2))*(F0a2R1-F0a2R2)*R1*J(1,a1,R1)*(1/a1 - 1/a2)
 
 
-norm=((2*np.pi*B2)/a2)*R2*F1a2R2+2*np.pi*R1*J(1,a1,R1)*(1/a1 - 1/a2)
+norm=((2*np.pi*B2)/abs(a2))*R2*F1a2R2+2*np.pi*R1*J(1,a1,R1)*(1/a1 - 1/a2)
 
-#W1=((J(0,a1,R1)**2 + J(1,a1,R1)**2)
-#	-(1/a1)*J(0,a1,R1)*J(1,abs(a2),R1))
-#W2= ((B2**2)*R2**2)*((F0a2R2**2 +F1a2R2**2 -
-#	(R2/a2)*F0a2R2*F1a2R2))
-#W3=((F0a2R1**2 +F1a2R1**2)+F0a2R1*F1a2R1*(1/a2))
+K=K1+K2-K3+K4
 
-K=+K1+K2-K3+K4
-Knorm=K/(norm**2)
-#W=W1+W2+W3
-
-#fig=plt.figure()
-#ax=fig.gca(projection='3d')
-#surf=ax.plot_surface(a1,a2,K, cmap=cm.coolwarm,
-#	linewidth=0,antialiased=False)
-
-#ax.zaxis.set_major_locator(LinearLocator(10))
-#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-#surf=ax.plot_surface(a1,a2,W,cmap=cm.coolwarm,
-	#linewidth=0,antialiased=False)
-#ax.set_xlabel('a1')
-#ax.set_ylabel('a2')
-#ax.set_zlabel('K')
-#Add color bar
-#fig.colorbar(surf,shrink=0.5,aspect=5) 
-#ax.set_zlim(0, 2.5)
-#plt.ylim(-4,4)
-#plt.xlim(3,0)
+plt.xlabel('a')
+plt.ylabel('K')
 plt.plot(a1,K)
+plt.ylim(-10,20)
 plt.show()
